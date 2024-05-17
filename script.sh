a5c2115db3fc5dad9a3a11abc83e92a12c81fe8b
Pri-shi <86788461+Pri-shi@users.noreply.github.com>
2024-05-17 10:22:04 +0000

    cadding script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if at least one argument is provided
+if [ $# -lt 1 ]; then
+    echo "No Ip Address Provided "
+    exit 1
+fi
+
+# Set the username and the path to the SSH key on the local machine
+username=root
+ssh_key_path=~/.ssh/id_rsa.pub
+
+# Set the list of remote servers using the arguments provided
+servers=("$@")
+
+# Loop through the servers and copy the SSH key
+for server in "${servers[@]}"
+do
+    echo "Copying SSH key to $server..."
+    ssh-copy-id -i "$ssh_key_path" "$username@$server"
+done
+
