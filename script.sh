3905dab7f869d5db868520fbad5f769845120d07
Pri-shi <86788461+Pri-shi@users.noreply.github.com>
2024-05-22 11:16:47 +0000

    modifiying script.sh

--- script.sh
+++ script.sh
@@ -10,16 +10,18 @@ fi
 username=$1
 password=$2
 ip=$3
-ssh_key_path='~/.ssh/id_rsa'
-ssh_key_pub_path='~/.ssh/id_rsa.pub'
+# ssh_key_path='~/.ssh/id_rsa'
+# ssh_key_pub_path='~/.ssh/id_rsa.pub'
 echo $ssh_key_path
 
 echo "STARTING ssh-keygen "
-ssh-keygen -t rsa -f $ssh_key_path -N ''
+//ssh-keygen -t rsa -f $ssh_key_path -N ''
+ssh-keygen -t rsa -N ''
 echo "keygen COMPLETED"
 
 echo "Copying SSH key to $3..."
-sshpass -p $2 ssh-copy-id -i $ssh_key_pub_path $1@$3
+//sshpass -p $2 ssh-copy-id -i $ssh_key_pub_path $1@$3
+sshpass -p $2 ssh-copy-id $1@$3
 
 echo "COPYING SSHID COMPLETED"
 
