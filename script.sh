77d986c00ef46318d9e7387bc468b33a30f3758a
Pri-shi <86788461+Pri-shi@users.noreply.github.com>
2024-05-22 11:42:28 +0000

    modifiying script.sh

--- script.sh
+++ script.sh
@@ -10,18 +10,18 @@ fi
 username=$1
 password=$2
 ip=$3
-# ssh_key_path='~/.ssh/id_rsa'
+ssh_key_path='/var/jenkins_home/.ssh/id_rsa'
 ssh_key_pub_path='/var/jenkins_home/.ssh/id_rsa'
 echo $ssh_key_path
 
 echo "STARTING ssh-keygen "
 #ssh-keygen -t rsa -f $ssh_key_path -N ''
-ssh-keygen -t rsa -N ''
+ssh-keygen -t rsa -N '' -f $ssh_key_path
 echo "keygen COMPLETED"
 
 echo "Copying SSH key to $3..."
 #sshpass -p $2 ssh-copy-id -i $ssh_key_pub_path $1@$3
-sshpass -p $2 ssh-copy-id -i "$ssh_key_pub_path" $1@$3
+sshpass -p $2 ssh-copy-id -i "$ssh_key_path" $1@$3
 
 echo "COPYING SSHID COMPLETED"
 
