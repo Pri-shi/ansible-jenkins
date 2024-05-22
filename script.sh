430b20a2f64359de6fd78237149592c2d1371aaa
Pri-shi <86788461+Pri-shi@users.noreply.github.com>
2024-05-22 09:44:42 +0000

    modifiying script.sh

--- script.sh
+++ script.sh
@@ -7,24 +7,17 @@ if [ $# -lt 2 ]; then
 fi
 
 # Set the username and the path to the SSH key on the local machine
-username=root
-ssh_key_file_name='~/.ssh/id_rsa'$3
-ssh_key_path='~/.ssh/id_rsa'$3'.pub'
+username=$3
+password=$2
+ip=$1
+ssh_key_path='~/.ssh/id_rsa.pub'
 echo $ssh_key_path
 
-# Set the list of remote servers using the arguments provided
-pass=$2
-echo "pass $2"
-# Shift the first argument off the array
-shift
+echo "STARTING ssh-keygen -r $3 "
+ssh-keygen -R $3
+echo "keygen COMPLETED"
 
-# Now $@ contains all arguments except the first one
-echo "server $3"
-echo "ssh-keygen -f $ssh_key_file_name -r $3 "
-ssh-keygen -f $ssh_key_file_name -R $3
-echo "keygen completed"
-echo "starting for $3..."
 echo "Copying SSH key to $3..."
-echo "ssh-copy-id -i $ssh_key_path  $2@$3"
-sshpass $2 | ssh-copy-id -i "$ssh_key_path" "$2@$3"
+echo "ssh-copy-id -i $ssh_key_path  $2@$1"
+sshpass $2 | ssh-copy-id -i "$ssh_key_path" "$2@$1"
 
